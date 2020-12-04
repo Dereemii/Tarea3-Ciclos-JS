664da7f8d47b59502d610cb0460cb051d0fe4e97
Leslie <leshermira@gmail.com>
2020-12-03 21:59:12 -0300

    alerta dia 7

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
 let dia = ["Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado", "Domingo"];
 
 for(let i = 1; i <= dia.length; i++ ){
+    if(i == 7){
+        alert(`El día en posición ${i} es el 7 :D`);
+        console.log(`El día en posición ${i} es el 7 :D`)
+    }
     if(i%2 === 0){
-        console.log(` El día en posición ${i} es número par`)
+        console.log(` El día en posición ${i}, es número par`)
     } else{
-        console.log(` El día en posición ${i} es número impar`)
+        console.log(` El día en posición ${i}, es número impar`)
     }
 }
